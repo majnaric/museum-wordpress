7f6638a5a26d4429826fc3c382e0ed7bbfc3ec68
Gox <majnaric@gmail.com>
2022-02-14 16:12:27 +0100

    fixing styling errors

--- archive-event.php
+++ archive-event.php
@@ -49,6 +49,7 @@ the_post();
     <div class="events-section__date-and-text">
       <div class="events-section__single-events-title">
         <a class="page-links" href="<?php the_permalink(); ?>"><h2 class="events-section__section-title events-section__section-title--archive-events custom-underline"><?php the_title(); ?></h2></a>
+        <p class="events-section__content-archive-events"><?php echo wp_trim_words(get_the_content(), 18); ?><a class="btn__learn-more btn__learn-more--dark" href="<?php the_permalink(); ?>"> Learn more</a></p>
       </div>
 
   
@@ -57,7 +58,6 @@ the_post();
         echo $eventDate-> format('d M');
         ?></p>
       </div>          
-          <p class="events-section__content-archive-events"><?php echo wp_trim_words(get_the_content(), 18); ?><a class="btn__learn-more btn__learn-more--dark" href="<?php the_permalink(); ?>"> Learn more</a></p>
     </div>
   </div>
 </div>
